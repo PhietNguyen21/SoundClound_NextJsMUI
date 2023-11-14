b9e768d942bca9431f97d6b2b43e5683652968d2
Nphiet <phietnguyen321@gmail.com>
2023-11-14 10:16:27 +0700

    wrapper fetch  APi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,31 @@
 
 import MainSlider from "@/components/main/main.slider";
 import Container from "@mui/material/Container/Container";
+import { sendRequestJS } from '../utils/old.api';
+const HomePage = async () => {
+  // const res = await fetch('http://localhost:8000/api/v1/tracks/top', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-type': 'application/json',
+  //     // 'Authorization': `Bearer ${token}`, // notice the Bearer before your token
+  //   },
+  //   body: JSON.stringify({
+  //     category: 'CHILL',
+  //     limit: 10
+  //   })
+  // })
+  // const d = await res.json();
+  // console.log(d);
 
-const HomePage = () => {
+  const res = await sendRequestJS({
+    url: 'http://localhost:8000/api/v1/tracks/top',
+    method: 'POST',
+    body: {
+      category: 'CHILL',
+      limit: 1
+    }
+  })
+  console.log(res)
 
   return <Container>
     <MainSlider />
